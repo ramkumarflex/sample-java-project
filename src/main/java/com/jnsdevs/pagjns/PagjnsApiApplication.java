026d4219d27a4e29f2bc890e9f0a83261f1e6274
ramkumarflex <48803337+ramkumarflex@users.noreply.github.com>
2025-08-27 12:55:23 -0500

    first commit

---
+++ src/main/java/com/jnsdevs/pagjns/PagjnsApiApplication.java
@@ -0,0 +1,13 @@
+package com.jnsdevs.pagjns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagjnsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagjnsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jnsdevs/pagjns/PagjnsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jnsdevs.pagjns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagjnsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
